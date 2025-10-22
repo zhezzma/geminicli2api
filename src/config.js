ec700f11073e38fddfaf7e5123c5edce6a6591b5
zhepama <250994229@qq.com>
2025-10-22 20:24:43 +0800

    feat: 实现Google认证初始化逻辑并更新环境变量处理

--- src/config.js
+++ src/config.js
@@ -1,30 +1,51 @@
 import dotenv from 'dotenv';
 import process from 'process';
 import path from 'path';
-import { DEFAULT_GEMINI_MODEL, DEFAULT_GEMINI_EMBEDDING_MODEL } from '@google/gemini-cli-core';
+import { DEFAULT_GEMINI_MODEL, DEFAULT_GEMINI_EMBEDDING_MODEL, AuthType } from '@google/gemini-cli-core';
 // Load environment variables from .env file
 dotenv.config();
 
 
 //https://github.com/google-gemini/gemini-cli/blob/main/packages/core/src/code_assist/storage.ts
-//C:\Users\zhepa\.gemini\oauth_creds.json
-function getCustomOAuthCredsPath() {
-  return process.env.GOOGLE_APPLICATION_CREDENTIALS || '/app/oauth_creds.json'
+//https://github.com/google-gemini/gemini-cli/blob/main/packages/core/src/code_assist/oauth2.ts
+
+function InitGoogleAuthentication() {
+
+  if (process.env.GOOGLE_GENAI_USE_VERTEXAI) {
+    console.log(`当前使用的认证方式是${AuthType.USE_VERTEX_AI}:${process.env.GOOGLE_API_KEY}`)
+    return;
+  }
+
+  if (process.env.GEMINI_API_KEY) {
+    console.log(`当前使用的认证方式是${AuthType.USE_GEMINI}:${process.env.GEMINI_API_KEY}`)
+    return;
+  }
+
+  if (process.env.GOOGLE_GENAI_USE_GCA && process.env.GOOGLE_CLOUD_ACCESS_TOKEN) {
+    console.log(`当前使用的认证方式是${AuthType.LOGIN_WITH_GOOGLE}:${process.env.GOOGLE_CLOUD_ACCESS_TOKEN}`)
+    return;
+  }
+
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    //C:\Users\zhepa\.gemini\oauth_creds.json  ~/.gemini/oauth_creds.json
+    console.log(`当前使用的认证方式是${AuthType.LOGIN_WITH_GOOGLE}:${process.env.GOOGLE_APPLICATION_CREDENTIALS}`)
+    return;
+  }
+
+  //设置成docker的默认路径
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = '/app/oauth_creds.json';
+  console.log(`当前使用的认证方式是${AuthType.LOGIN_WITH_GOOGLE}:${process.env.GOOGLE_APPLICATION_CREDENTIALS}`)
 }
 
+InitGoogleAuthentication();
 
 const config = {
   // Server configuration
   port: process.env.PORT || 3000,
   host: process.env.HOST || '0.0.0.0',
   token: process.env.TOKEN,
- 
 
-  // Google authentication
-  //https://github.com/google-gemini/gemini-cli/blob/main/packages/core/src/code_assist/oauth2.ts
-  googleApplicationCredentials: getCustomOAuthCredsPath(),
-  googleGenaiUseGca: process.env.GOOGLE_GENAI_USE_GCA === 'true',
-  googleCloudAccessToken: process.env.GOOGLE_CLOUD_ACCESS_TOKEN,
+
 
   // CLI configuration
   cliVersion: process.env.CLI_VERSION || process.version,
